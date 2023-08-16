46e11ec954247caa908e9c1d2d3ec886722c9f7c
Joyna Mahendren <joyna.mahendren@bcstechnology.com.au>
2023-08-16 12:01:40 +0530

    About

--- my-app/src/routes/About.js
+++ my-app/src/routes/About.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import Heroimg2 from '../components/Heroimg2';
 
 const About = () => {
   return (
     <div>
      <Navbar />
+     <Heroimg2 heading= "ABOUT." text="I'm a friendly front-end Developer" />
      <Footer />
     </div>
   )
